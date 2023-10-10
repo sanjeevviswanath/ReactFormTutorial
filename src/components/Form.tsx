4644d3437dbe1f42b774b13dc0eb4415027af8d0
Sanjeev <sanjeev.viswanath@gmail.com>
2023-10-09 22:28:39 -0500

    Adding initial form tutorial project file

---
+++ src/components/Form.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form action="">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
