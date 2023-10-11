5f8ceb9397d7b662886b4d73b1fbaa24bb370d02
Sanjeev <sanjeev.viswanath@gmail.com>
2023-10-10 21:48:34 -0500

    added submit button disabled feature if form is invalid

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,15 +3,25 @@
 // import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: String;
+  age: number;
+}
 const Form = () => {
-  const { register, handleSubmit, setFocus, resetField } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    setFocus,
+    resetField,
+  } = useForm<FormData>();
   const onFormSubmit = (data: FieldValues) => {
     console.log(data);
     resetField("name");
     resetField("age");
     setFocus("name");
   };
-
+  //   console.log(formState.errors);
   return (
     <form onSubmit={handleSubmit(onFormSubmit)}>
       <div className="mb-3">
@@ -19,24 +29,38 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field must be at least 3 characters
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true, valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type === "required" && (
+          <p className="text-danger">The age field is required</p>
+        )}
+        {errors.age?.type === "valueAsNumber" && (
+          <p className="text-danger">The age field must be a number</p>
+        )}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
