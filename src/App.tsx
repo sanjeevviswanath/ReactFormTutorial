ca1fae3d203a6c56308132b7b51544cd9934336b
Sanjeev <sanjeev.viswanath@gmail.com>
2023-10-12 20:32:53 -0500

    In the expenselist component added table. updated app componet with dummy expenses data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 // import FormUseRef from "./components/FormuseRef";
 // import FormuseState from "./components/FormuseState";
-import ExpenseList from "./components/ExpenseList";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 import Form from "./components/Form";
-
+const expenses = [
+  {
+    id: 1,
+    description: "Milk",
+    amount: 5,
+    category: "Groceries",
+  },
+  {
+    id: 2,
+    description: "Bread",
+    amount: 2,
+    category: "Groceries",
+  },
+];
 function App() {
   return (
     <>
       {/* <FormUseRef></FormUseRef>
       <FormuseState></FormuseState>
       <Form></Form> */}
-      <ExpenseList></ExpenseList>
+      <ExpenseList expenses={expenses}></ExpenseList>
     </>
   );
 }
---
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -0,0 +1,37 @@
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+interface Props {
+  expenses: Expense[];
+}
+const ExpenseList = ({ expenses }: Props) => {
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button className="btn btn-outline-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseList;
