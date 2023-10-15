833ebc1a649c59a28c835a539293fffddbdb43ec
Sanjeev <sanjeev.viswanath@gmail.com>
2023-10-14 20:09:07 -0500

    commented unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
-//import Form from "./components/Form";
+// import Form from "./components/Form";
 //import categories from "./expense-tracker/categories";
 import { useState } from "react";
-import ProductList from "./components/ProductList";
-// const expenses = ;
+//import ProductList from "./components/ProductList";
+
 type Expense = {
   id: number;
   category: "Groceries" | "Utilities" | "Entertainment";
@@ -15,7 +15,7 @@ type Expense = {
   amount: number;
 };
 function App() {
-  const [selectdProdCategory, setSelectedProdCategory] = useState("");
+  // const [selectdProdCategory, setSelectedProdCategory] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState<Expense[]>([
     // {
